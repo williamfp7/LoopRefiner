42c5da553405abf6199c9418eee77e9ec731326a
William Porto <williamfp7@yahoo.com.br>
2021-04-05 17:05:14 -0300

    Add files via upload

--- com/portoreports/Object.pm
+++ com/portoreports/Object.pm
@@ -1,1 +1,28 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
 
+package Object;
+
+sub new{
+    my $class=shift;
+    my ($self)={@_};
+    bless $self,$class;
+    return $self;
+}
+
+sub toString{
+    my $self=shift;
+    return $self;
+}
+
+sub equals{
+    my $self=shift;
+    my ($obj)=@_;
+    if($self==$obj){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+1;
