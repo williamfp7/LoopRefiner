a670ec737d316d24666a0c0421859f89f1c3cfec
William Porto <williamfp7@yahoo.com.br>
2021-04-11 12:50:12 -0300

    Update main.pl

--- main.pl
+++ main.pl
@@ -5,16 +5,16 @@ use lib "com/portoreports/LoopRefiner/";
 use LoopRefiner;
 
 my $a=LoopRefiner->new(
-    method=>"QMEAN",
-    targetScore=>-2,
-    pdb=>"model.pdb",
-    window=>9,
-    modellerVersion=>'10.1',
-    totalModels=>100,
+    method=>"QMEAN",    #Only QMEAN is available at the moment
+    targetScore=>-2,    #The threshold to reach
+    pdb=>"model.pdb",   #The initial model
+    window=>9,          #The window lengh to be refined (e.g. if the target is the 15th residue, the window starts at 11th and ends at 19th residue)
+    modellerVersion=>'10.1', #The modeller version
+    totalModels=>100,   #The total models to perform for each round of refinement; it exhausted, the value is incremented by 10 on next iteration
     #Optional settings
-    #prefix=>"MyModel",
-    iteration=>1,
-    email=>'youremail@yourserver.com'
+    #prefix=>"MyModel", #The prefix of the saved models
+    iteration=>1,       #The number of current best model
+    email=>'youremail@yourserver.com' #Your e-mail
 );
 
 $a->load();
